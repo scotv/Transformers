3bb928c9ec7cecdbadc2aa34c78a6fe6eb93d948
Scott.L <scott.l@xg>
2013-12-04 21:10:52 +0800

    introduce express 3
    update to app.get()

--- demo/socket-app.js
+++ demo/socket-app.js
@@ -1,24 +1,15 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs');
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-app.listen(80);
+server.listen(80);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { message: 'hello socket.io', error: '' });
+  socket.emit('news', { message: 'hello socket.io', error: '', time: (new Date()).toISOString() });
   socket.on('my other event', function (data) {
     console.log('my other event', data);
   });
